fe60879d9372f288c4ef039644ec760154072c82
U-235-92 <a745296@yandex.ru>
2024-07-27 18:05:36 +0400

    Empty project

---
+++ src/test/java/com/other/app/OtherSpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.other.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherSpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
